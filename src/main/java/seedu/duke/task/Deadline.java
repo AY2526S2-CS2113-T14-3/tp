35025293598fd358487a54faa97c27144ef1cce1
Wen Jun Yu <e1406517@u.nus.edu>
2026-03-15 12:51:30 +0800

    Checkstyle corrections

--- src/main/java/seedu/duke/task/Deadline.java
+++ src/main/java/seedu/duke/task/Deadline.java
@@ -1,7 +1,6 @@
 package seedu.duke.task;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
