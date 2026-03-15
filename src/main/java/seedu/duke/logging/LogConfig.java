3ad97f676c3bfe20fe7f558f623569469e71d4ef
Wen Jun Yu <e1406517@u.nus.edu>
2026-03-15 21:04:30 +0800

    Added logconfig class

---
+++ src/main/java/seedu/duke/logging/LogConfig.java
@@ -0,0 +1,23 @@
+package seedu.duke.logging;
+
+import java.util.logging.*;
+import java.io.IOException;
+
+public class LogConfig {
+    public static void setup() {
+        LogManager.getLogManager().reset();
+        Logger rootLogger = Logger.getLogger("");
+        rootLogger.setLevel(Level.ALL);
+
+        try {
+            // File Handler: Saves EVERYTHING to 'unitasker.log'
+            FileHandler fileHandler = new FileHandler("unitasker.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            fileHandler.setLevel(Level.ALL);
+            rootLogger.addHandler(fileHandler);
+
+        } catch (IOException e) {
+            System.err.println("Logging system failed to initialize: " + e.getMessage());
+        }
+    }
+}
